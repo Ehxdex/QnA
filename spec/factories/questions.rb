3e9e330d90e3d66b6fdccfa1f6a99805381b029b
rav <reznichenkoav@me.com>
2025-04-04 10:23:23 +0300

    Fix: tests

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -6,5 +6,14 @@ FactoryBot.define do
     trait :invalid do
       title { nil }
     end
+
+    trait :with_attachment do
+      after(:create) do |question|
+        question.files.attach(
+          io: File.open(Rails.root.join('spec', 'rails_helper.rb')),
+          filename: 'rails_helper.rb'
+        )
+      end
+    end
   end
 end
