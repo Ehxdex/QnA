b055ed82361523ce45a937d799ad118f908e8c9b
rav <reznichenkoav@me.com>
2025-04-21 10:26:54 +0300

    Add links url validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,8 @@
 class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
-  validates :name, :url, presence: true
+  validates :name, presence: true
+
+  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+  validates :url, format: { with: URL_REGEXP, message: "is invalid (format must be 'http(https)://google.com')" }
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+
   it { should belong_to :linkable }
   it { should validate_presence_of :name }
-  it { should validate_presence_of :url }
+
+  it { should validate_presence_of(:url).with_message("is invalid (format must be 'http(https)://google.com')") }
+
+  it { should allow_value('https://foo.com', 'https://bar.com').for(:url) }
+  it { should_not allow_values('https://googlecom', 'http///google.com', 'google.com', 'google').for(:url) }
 end
